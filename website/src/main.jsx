08af96273bbd8385fba83930fd097cc7e6f588e3
Nguyen Van Tin <bguyt46@gmail.com>
2024-03-17 09:01:51 +0700

    build: add layout

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HelmetProvider>
+    <BrowserRouter>
+      <Suspense>
+        <App />
+      </Suspense>
+    </BrowserRouter>
+  </HelmetProvider>
 )
---
+++ website/src/pages/ChatIndex.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ChatIndex = () => {
+  return (
+    <div>ChatIndex</div>
+  )
+}
