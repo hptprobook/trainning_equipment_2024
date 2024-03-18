0a3fc69e7b630ccbe01a00d7ad0e88ebe4913d22
Phan Thanh Hoa <hptprobook@gmail.com>
2024-03-18 10:25:33 +0700

    Complete compiler api

--- server/src/executeLibs/index.js
+++ server/src/executeLibs/index.js
@@ -1,6 +1,7 @@
 import { exec } from 'child_process';
+import fs from 'fs';
 
-const executeCommand = (command) => {
+const executeCommand = (command, filePath) => {
   return new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
       if (error) {
@@ -10,20 +11,23 @@ const executeCommand = (command) => {
       } else {
         resolve(stdout);
       }
+      fs.unlink(filePath, (unlinkError) => {
+        if (unlinkError) reject(new Error(`Unlink error: ${unlinkError.message}\n`));
+      });
     });
   });
 };
 
 const executePy = (filePath) => {
-  return executeCommand(`python "${filePath}"`);
+  return executeCommand(`python "${filePath}"`, filePath);
 };
 
 const executeJs = (filePath) => {
-  return executeCommand(`node "${filePath}"`);
+  return executeCommand(`node "${filePath}"`, filePath);
 };
 
 const executePhp = (filePath) => {
-  return executeCommand(`php "${filePath}"`);
+  return executeCommand(`php "${filePath}"`, filePath);
 };
 
 export const executeFile = { executePy, executeJs, executePhp };
