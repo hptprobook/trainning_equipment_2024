2a8450ddf4c9e76015af21e3b8f8b6d3cdc9f65e
Phan Thanh Hoa <hptprobook@gmail.com>
2024-03-19 19:02:03 +0700

    Fix bug, code UI, Check AI compiler

--- website/src/layout/chat/ChatLayout.jsx
+++ website/src/layout/chat/ChatLayout.jsx
@@ -6,33 +6,38 @@ import { useResponsive } from '~/config/reponsiveConfig';
 import Header from './header';
 import NavChat from './nav';
 import { NAV_WIDTH } from './layoutConfig';
+import { useLocation } from 'react-router-dom';
 
 const drawerWidth = NAV_WIDTH;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open, headerheight }) => ({
-    height: '100vh',
-    backgroundColor: theme.palette.background.paper,
-    flexGrow: 1,
-    paddingTop: headerheight,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawerWidth}px`,
-    ...(open && {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-    }),
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(({ theme, open, headerheight }) => ({
+  height: '100vh',
+  backgroundColor: theme.palette.background.paper,
+  flexGrow: 1,
+  paddingTop: headerheight,
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
   }),
-);
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
 export default function ChatLayout({ children }) {
   const [open, setOpen] = React.useState(true);
   const [headerHeight, setHeaderHeight] = React.useState(0);
   const heightRef = React.useRef(null);
+
+  /* Code từ Compiler */
+  const location = useLocation();
+  const { sourceCode } = location.state || {};
+  console.log(sourceCode);
+
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -66,5 +71,5 @@ export default function ChatLayout({ children }) {
   );
 }
 ChatLayout.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
 };
--- website/src/pages/Compiler/CompilerPage.jsx
+++ website/src/pages/Compiler/CompilerPage.jsx
@@ -1,30 +1,39 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Editor, { useMonaco } from '@monaco-editor/react';
 import { convertLanguage, defaultCode } from '~/utils/formatters';
 import { runOnlineCompiler } from '~/APIs';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
+import Slider from '@mui/material/Slider';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
-const HEADER_HEIGHT = '48px';
+const HEADER_HEIGHT = '56px';
 const CONTAINER_HEIGHT = `calc(100% - ${HEADER_HEIGHT})`;
 
 const CompilerPage = () => {
+  const monaco = useMonaco();
+  const editorRef = useRef(null); // Ref cho trình soạn thảo
+  const navigate = useNavigate();
   const [sourceCode, setSourceCode] = useState(defaultCode.javascript);
   const [selectedLanguage, setSelectedLanguage] = useState('javascript');
   const [isCompiling, setIsCompiling] = useState(false);
   const [compileOutput, setCompileOutput] = useState('');
-  const editorRef = useRef(null); // Ref cho trình soạn thảo
-  const monaco = useMonaco();
+  const [theme, setTheme] = useState('light');
+  const [editorFontSize, setEditorFontSize] = useState(15);
+
+  const handleCheckAI = () => {
+    navigate('/chat', { state: { sourceCode } });
+  };
 
   /* eslint-disable-next-line no-unused-vars */
   const handleEditorChange = (value, event) => {
     setSourceCode(value);
   };
 
-  // Chuyển đổi ngôn ngữ cho Editor khi selectedLanguage thay đổi
   useEffect(() => {
     if (editorRef.current) {
       const editor = editorRef.current;
@@ -40,8 +49,9 @@ const CompilerPage = () => {
       editor.setSelection(range);
 
       editor.getAction('editor.action.formatDocument').run();
+      monaco.editor.setTheme(theme === 'light' ? 'vs' : 'vs-dark');
     }
-  }, [selectedLanguage, monaco]);
+  }, [selectedLanguage, monaco, theme]);
 
   const handleRunCode = () => {
     const languageForServer = convertLanguage(selectedLanguage);
@@ -54,6 +64,24 @@ const CompilerPage = () => {
         setCompileOutput('Error running code: ' + error.response.data.error);
       })
       .finally(() => setIsCompiling(false));
+  };
+
+  const handleCopyCode = () => {
+    if (navigator.clipboard) {
+      // Kiểm tra xem API clipboard có sẵn không
+      navigator.clipboard
+        .writeText(sourceCode)
+        .then(() => {
+          toast.success('Copied!', {
+            autoClose: 500,
+          });
+        })
+        .catch((err) => {
+          alert('Failed to copy code: ', err);
+        });
+    } else {
+      alert('Clipboard API not available.');
+    }
   };
 
   return (
@@ -73,13 +101,20 @@ const CompilerPage = () => {
           justifyContent: 'space-between',
           alignItems: 'center',
           px: 4,
-          borderBottom: '1px solid #333',
+          bgcolor: theme === 'light' ? '#eeeeee' : '#424242',
         }}
       >
-        <Typography variant="body1" color="initial">
+        <Typography variant="body1" color={theme === 'light' ? 'inherit' : '#fff'}>
           SLIF Online Compiler
         </Typography>
-        <Link to={'/chat'}>Chat Box </Link>
+        <Link
+          to={'/chat'}
+          style={{
+            color: theme === 'light' ? '#333' : '#fff',
+          }}
+        >
+          Chat Box{' '}
+        </Link>
       </Box>
 
       {/* ========== CONTAINER ========== */}
@@ -89,12 +124,18 @@ const CompilerPage = () => {
           height: CONTAINER_HEIGHT,
         }}
       >
-        <Grid container spacing={0}>
+        <Grid
+          container
+          spacing={0}
+          sx={{
+            bgcolor: theme === 'light' ? 'fff' : '#1e1e1e',
+          }}
+        >
           <Grid
             item
             xs={7}
             sx={{
-              borderRight: '1px solid #333',
+              borderRight: '1px solid #c9c6c6',
             }}
           >
             <Box
@@ -105,27 +146,121 @@ const CompilerPage = () => {
                 justifyContent: 'space-between',
                 alignItems: 'center',
                 px: 4,
-                borderBottom: '1px solid #333',
+                borderBottom: '1px solid #c9c6c6',
               }}
             >
               <Box
                 sx={{
-                  gap: 2,
+                  display: 'flex',
+                  gap: 3,
+                  alignItems: 'center',
                 }}
               >
-                Language:
-                <Button onClick={() => setSelectedLanguage('javascript')}>Js</Button>
-                <Button onClick={() => setSelectedLanguage('php')}>PHP</Button>
-                <Button onClick={() => setSelectedLanguage('python')}>Py</Button>
-                <Button onClick={() => setSelectedLanguage('cpp')}>C++</Button>
+                <Box
+                  sx={{
+                    cursor: 'pointer',
+                    '&:hover': {
+                      opacity: 0.8,
+                    },
+                  }}
+                  onClick={() => setSelectedLanguage('javascript')}
+                >
+                  <img
+                    width={30}
+                    style={{
+                      border: selectedLanguage === 'javascript' ? '2px solid #ba000d' : 'none',
+                    }}
+                    height={30}
+                    alt="JavaScript"
+                    src={'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Javascript_badge.svg/946px-Javascript_badge.svg.png'}
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    cursor: 'pointer',
+                    '&:hover': {
+                      opacity: 0.8,
+                    },
+                  }}
+                  onClick={() => setSelectedLanguage('python')}
+                >
+                  <img
+                    style={{
+                      border: selectedLanguage === 'python' ? '2px solid #ba000d' : 'none',
+                    }}
+                    width={30}
+                    height={30}
+                    alt="Python"
+                    src={'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/640px-Python.svg.png'}
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    cursor: 'pointer',
+                    '&:hover': {
+                      opacity: 0.8,
+                    },
+                  }}
+                  onClick={() => setSelectedLanguage('cpp')}
+                >
+                  <img
+                    style={{
+                      border: selectedLanguage === 'cpp' ? '2px solid #ba000d' : 'none',
+                    }}
+                    width={30}
+                    height={30}
+                    alt="C++"
+                    src={'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png'}
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    cursor: 'pointer',
+                    '&:hover': {
+                      opacity: 0.8,
+                    },
+                  }}
+                  onClick={() => setSelectedLanguage('php')}
+                >
+                  <img
+                    style={{
+                      border: selectedLanguage === 'php' ? '2px solid #ba000d' : 'none',
+                    }}
+                    width={30}
+                    height={30}
+                    alt="PHP"
+                    src={'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/800px-PHP-logo.svg.png'}
+                  />
+                </Box>
+              </Box>
+              <Box sx={{ width: 150 }}>
+                <Slider
+                  defaultValue={15}
+                  onChange={(e) => setEditorFontSize(e.target.value)}
+                  valueLabelDisplay="auto"
+                  shiftStep={3}
+                  step={1}
+                  marks
+                  min={13}
+                  max={25}
+                />
               </Box>
               <Box
                 sx={{
+                  display: 'flex',
                   gap: 2,
                 }}
               >
-                <Button>Save</Button>
-                <Button onClick={handleRunCode} disabled={isCompiling}>
+                <Button variant="contained" size="small" onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+                  {theme === 'light' ? 'dark' : 'light'}
+                </Button>
+                <Button onClick={handleCopyCode} variant="contained" size="small">
+                  Copy
+                </Button>
+                <Button variant="contained" size="small">
+                  Save
+                </Button>
+                <Button variant="contained" size="small" onClick={handleRunCode} disabled={isCompiling}>
                   Run
                 </Button>
               </Box>
@@ -141,6 +276,7 @@ const CompilerPage = () => {
                   vertical: 'hidden',
                   verticalScrollbarSize: 0,
                 },
+                fontSize: editorFontSize,
               }}
             />
           </Grid>
@@ -153,26 +289,43 @@ const CompilerPage = () => {
                 justifyContent: 'space-between',
                 alignItems: 'center',
                 px: 4,
-                borderBottom: '1px solid #333',
+                borderBottom: '1px solid #c9c6c6',
                 overflowY: 'auto',
+                bgcolor: theme === 'light' ? 'fff' : '#1e1e1e',
               }}
             >
-              <Typography>Output</Typography>
-              <Box>
-                <Button onClick={() => setCompileOutput('')}>Clear</Button>
-                <Button>Check AI</Button>
-              </Box>
-            </Box>
-            {isCompiling && <p>Compiling...</p>}
-            {compileOutput && (
-              <Typography
+              <Typography color={theme === 'light' ? 'inherit' : '#fff'}>Output</Typography>
+              <Box
                 sx={{
-                  whiteSpace: 'pre-wrap',
+                  display: 'flex',
+                  gap: 2,
                 }}
               >
-                {compileOutput}
-              </Typography>
-            )}
+                <Button size="small" variant="contained" onClick={() => setCompileOutput('')}>
+                  Clear
+                </Button>
+                <Button onClick={handleCheckAI} size="small" variant="contained">
+                  Check AI
+                </Button>
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                px: 4,
+                py: 2,
+              }}
+            >
+              {isCompiling && <p>Compiling...</p>}
+              {compileOutput && (
+                <Typography
+                  sx={{
+                    whiteSpace: 'pre-wrap',
+                  }}
+                >
+                  {compileOutput}
+                </Typography>
+              )}
+            </Box>
           </Grid>
         </Grid>
       </Box>
