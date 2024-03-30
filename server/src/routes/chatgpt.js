6cb66c778f016a93f9e27583097b516fc519c93f
beovan <truongvangiap04@gmail.com>
2024-03-30 13:55:02 +0700

    Refactor: GPT controllers and remove unused code

--- server/src/routes/chatgpt.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { chatGptController } from '../controllers/chatgptController';
-
-const router = express.Router();
-
-router.post('/chatgpt/chat', chatGptController);
-
-export default router;
