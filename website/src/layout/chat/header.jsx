a6298f977794f1548519e47db198ae7410ca84ac
Nguyen Van Tin <bguyt46@gmail.com>
2024-04-04 13:06:36 +0700

    feat: chip status user

--- website/src/layout/chat/header.jsx
+++ website/src/layout/chat/header.jsx
@@ -8,86 +8,93 @@ import { useTheme } from '@emotion/react';
 import MenuIcon from '@mui/icons-material/Menu';
 
 import PropTypes from 'prop-types';
-import { Button, IconButton, Toolbar, Typography } from '@mui/material';
+import { Button, IconButton, Stack, Toolbar, Typography } from '@mui/material';
 import { NAV_WIDTH } from './layoutConfig';
 import { Link } from 'react-router-dom';
 
 const drawerWidth = NAV_WIDTH;
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+	shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
+	transition: theme.transitions.create(['margin', 'width'], {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+	...(open && {
+		width: `calc(100% - ${drawerWidth}px)`,
+		marginLeft: `${drawerWidth}px`,
+		transition: theme.transitions.create(['margin', 'width'], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	}),
 }));
 
 const Header = React.forwardRef(({ open, handleDrawerOpen }, ref) => {
-  const theme = useTheme();
-  return (
-    <AppBar position="fixed" open={open} ref={ref}
-      sx={{
-        boxShadow: 'none'
-      }}>
-      <Toolbar
-        sx={{
-          backgroundColor: theme.palette.background.paper,
-          // boxShadow: theme.shadows,
-        }}
-      >
-        <IconButton
-          aria-label="open menu"
-          onClick={handleDrawerOpen}
-          edge="start"
-          sx={{
-            mr: 2,
-            ...(open && { display: 'none' }),
-            color: theme.palette.text.primary
-          }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Button
-          sx={{
-            color: theme.palette.text.primary,
-            position: 'absolute',
-            right: '20px',
-          }}
-          size="medium">
-          <Link
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              textDecoration: 'none',
-              color: theme.palette.text.primary,
-            }}
-            to='/'>
-            <Typography
-              sx={{
-                fontWeight: 600,
-                lineHeight: 1.5,
-                marginTop: '3px',
-              }}>
-              Run Compiler
-            </Typography>
-            <ChevronRightIcon />
-          </Link>
-        </Button>
-      </Toolbar>
-    </AppBar >
-  );
+	const theme = useTheme();
+	return (
+		<AppBar
+			position="fixed"
+			open={open}
+			ref={ref}
+			sx={{
+				boxShadow: 'none',
+			}}
+		>
+			<Toolbar
+				sx={{
+					backgroundColor: theme.palette.background.paper,
+					// boxShadow: theme.shadows,
+				}}
+			>
+				<IconButton
+					aria-label="open menu"
+					onClick={handleDrawerOpen}
+					edge="start"
+					sx={{
+						mr: 2,
+						...(open && { display: 'none' }),
+						color: theme.palette.text.primary,
+					}}
+				>
+					<MenuIcon />
+				</IconButton>
+				<Button
+					sx={{
+						color: theme.palette.text.primary,
+						position: 'absolute',
+						right: '20px',
+					}}
+					size="medium"
+				>
+					<Link
+						style={{
+							display: 'flex',
+							alignItems: 'center',
+							textDecoration: 'none',
+							color: theme.palette.text.primary,
+						}}
+						to="/"
+					>
+						<Typography
+							sx={{
+								fontWeight: 600,
+								lineHeight: 1.5,
+								marginTop: '3px',
+							}}
+						>
+							Run Compiler
+						</Typography>
+						<ChevronRightIcon />
+					</Link>
+				</Button>
+			</Toolbar>
+		</AppBar>
+	);
 });
 Header.propTypes = {
-  open: PropTypes.bool,
-  handleDrawerOpen: PropTypes.func
+	open: PropTypes.bool,
+	handleDrawerOpen: PropTypes.func,
 };
 export default Header;
