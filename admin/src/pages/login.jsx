337e2a870c4611a2c1a78e2b0970be525ee16e59
beovan <truongvangiap04@gmail.com>
2024-04-04 12:41:51 +0700

    style: Commented out unused code and removed upgrade button from dashboard nav

--- admin/src/pages/login.jsx
+++ admin/src/pages/login.jsx
@@ -1,17 +1,17 @@
-import { Helmet } from 'react-helmet-async';
+// import { Helmet } from 'react-helmet-async';
 
-import { LoginView } from 'src/sections/login';
+// import { LoginView } from 'src/sections/login';
 
-// ----------------------------------------------------------------------
+// // ----------------------------------------------------------------------
 
-export default function LoginPage() {
-  return (
-    <>
-      <Helmet>
-        <title> Login | Minimal UI </title>
-      </Helmet>
+// export default function LoginPage() {
+//   return (
+//     <>
+//       <Helmet>
+//         <title> Login | Minimal UI </title>
+//       </Helmet>
 
-      <LoginView />
-    </>
-  );
-}
+//       <LoginView />
+//     </>
+//   );
+// }
